28a6d7b04732ff9b4c2c17e1e31da4d42b15a22f
yamadaab <s.yamada.1125@gmail.com>
2021-03-09 16:20:39 +0900

    remove unnecessary code

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,7 +3,4 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -3,7 +3,4 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
